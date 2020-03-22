f8619c4a2ba8163ca72a5761fb8413952269be11
Sonya Sosnovik <sonya_sosnovik>
2020-03-22 15:00:36 +0300

    MVC structure

---
+++ BookAndRent/BookAndRent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookAndRent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
