090de5ded3cb1661a6c7d44bc901f3b1841aa349
Sonya Sosnovik <sonya_sosnovik>
2020-03-25 20:57:39 +0300

    DB schema deployment

--- BookAndRent/BookAndRent/Program.cs
+++ BookAndRent/BookAndRent/Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BookAndRent.Models.Implementations;
+using BookAndRent.Models.Intefaces;
+using BookAndRent.Repository;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace BookAndRent
@@ -14,6 +18,39 @@ namespace BookAndRent
     {
         public static void Main(string[] args)
         {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<BookAndRentDBContext>();
+                    context.Database.EnsureCreated();
+                    context.Apartments.Add(
+                        new Apartment
+                        {
+                            Address = "addr",
+                            Title = "title",
+                            Comments = new List<IComment>
+                            {
+                                new Comment
+                                {
+                                    Content = "asdsad"
+                                }
+                            }
+                        });
+
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
             CreateWebHostBuilder(args).Build().Run();
         }
 
