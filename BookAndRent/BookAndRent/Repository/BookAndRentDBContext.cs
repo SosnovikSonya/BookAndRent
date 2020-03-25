c629bd3677652060f04e45418f92cbb2198cd8d5
Sonya Sosnovik <sonya_sosnovik>
2020-03-25 20:56:24 +0300

    DB context

---
+++ BookAndRent/BookAndRent/Repository/BookAndRentDBContext.cs
@@ -0,0 +1,26 @@
+﻿using BookAndRent.Models.Implementations;
+using BookAndRent.Models.Intefaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAndRent.Repository
+{
+    public class BookAndRentDBContext : DbContext
+    {
+        public BookAndRentDBContext(DbContextOptions<BookAndRentDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Apartment> Apartments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Apartment>().ToTable("Apartment");
+            modelBuilder.Entity<Comment>().ToTable("Comment");
+        }
+    }
+}
