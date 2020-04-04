566e4eab1fcdf569c184b197b183726b601573c1
Sonya Sosnovik <sonya_sosnovik>
2020-04-04 18:51:55 +0300

    Initial data insert

--- BookAndRent/BookAndRent/Program.cs
+++ BookAndRent/BookAndRent/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using BookAndRent.Models.Implementations;
-using BookAndRent.Models.Intefaces;
 using BookAndRent.Repository;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -23,31 +21,12 @@ namespace BookAndRent
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<BookAndRentDBContext>();
-                    context.Database.EnsureCreated();
-                    context.Apartments.Add(
-                        new Apartment
-                        {
-                            Address = "addr",
-                            Title = "title",
-                            Comments = new List<IComment>
-                            {
-                                new Comment
-                                {
-                                    Content = "asdsad"
-                                }
-                            }
-                        });
+                var context = services.GetRequiredService<BookAndRentDBContext>();
+                context.Database.EnsureCreated();
+                context.SaveChanges();
 
-                    context.SaveChanges();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
+                // Uncomment to insert demo data
+                // context.InitializeDemoData();
             }
 
             host.Run();
--- BookAndRent/BookAndRent/Repository/BookAndRentDBContext.cs
+++ BookAndRent/BookAndRent/Repository/BookAndRentDBContext.cs
@@ -1,6 +1,4 @@
-﻿using BookAndRent.Models.Implementations;
-using BookAndRent.Models.Intefaces;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +10,99 @@ namespace BookAndRent.Repository
     {
         public BookAndRentDBContext(DbContextOptions<BookAndRentDBContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Apartment> Apartments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<RentDate> RentDates { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Apartment>().ToTable("Apartment");
             modelBuilder.Entity<Comment>().ToTable("Comment");
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Picture>().ToTable("Picture");
+            modelBuilder.Entity<Facility>().ToTable("Facility");
+            modelBuilder.Entity<Contract>().ToTable("Contract");
+            modelBuilder.Entity<RentDate>().ToTable("RentDate");
+        }
+
+        public void InitializeDemoData()
+        {
+            Facilities.AddRange(
+                new Facility
+                {
+                    Title = "Интернет"
+                },
+                new Facility
+                {
+                    Title = "Фен"
+                }
+            );
+            
+            Users.AddRange(
+                new User
+                {
+                    FirstName = "Пупа"                    
+                },
+                new User
+                {
+                    FirstName = "Лупа"
+                });
+
+            SaveChanges();
+
+            Apartments.Add(
+                new Apartment
+                {
+                    Address = "улица пушкина дом Колотушкина",
+                    Description = "Description",
+                    RoomAmount = 3,
+                    SleepingPlaces = 5,
+                    CostPerNight = 100,
+                    Title = "Лучший бордель того города",
+                    Coordinates = "zhopa",
+                    UserId = Users.ToList().Single(user => user.FirstName == "Пупа").UserId,
+                    AvailableDates = new List<RentDate>
+                    {
+                        new RentDate
+                        {
+                            StartDate = DateTime.UtcNow,
+                            EndDate = DateTime.UtcNow.AddDays(30)
+                        }
+                    },
+                    ApartmentFacilities = new List<ApartmentFacility>
+                    {
+                        new ApartmentFacility
+                        {
+                            FacilityId = Facilities.ToList()[0].FacilityId
+                        }
+                    },
+                    Comments = new List<Comment>
+                    {
+                        new Comment
+                        {
+                            Content = "hueta",
+                            UserId = Users.ToList()[1].UserId,
+                            Date = DateTime.UtcNow                             
+                        }
+                    }
+                });
+            Contracts.Add(new Contract
+            {
+                ApartmentId = Apartments.ToList()[0].ApartmentId,
+                CheckIn = DateTime.UtcNow.AddDays(1),
+                CheckOut = DateTime.UtcNow.AddDays(4),
+                ContractDate = DateTime.UtcNow,
+                Holder = Users.ToList()[0],
+                Renter  = Users.ToList()[1],
+            });
+            SaveChanges();
         }
     }
 }
