941d466f2b1dc8d11033ea6e2dcbb5b4b8a08cd7
Sonya Sosnovik <sonya_sosnovik>
2020-04-14 16:52:35 +0300

    Using updated

--- BookAndRent/BookAndRent/Program.cs
+++ BookAndRent/BookAndRent/Program.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using BookAndRent.Repository;
+﻿using BookAndRent.Repository.SqlRepository;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace BookAndRent
 {
