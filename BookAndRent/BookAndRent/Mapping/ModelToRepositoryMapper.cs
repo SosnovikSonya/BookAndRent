029e5ba29863c583aac3b5fdb50f7fa4bfbd3487
Sonya Sosnovik <sonya_sosnovik>
2020-04-27 22:54:37 +0300

    update mapping

--- BookAndRent/BookAndRent/Mapping/ModelToRepositoryMapper.cs
+++ BookAndRent/BookAndRent/Mapping/ModelToRepositoryMapper.cs
@@ -2,6 +2,7 @@
 using BookAndRent.DependencyResolving;
 using BookAndRent.Models.Intefaces;
 using BookAndRent.Repository.SqlRepository;
+using System.Linq;
 
 namespace BookAndRent.Mapping
 {
@@ -15,6 +16,38 @@ namespace BookAndRent.Mapping
             CreateMap<User, IUser>()
                 .ConstructUsing(dbEntity => DependencyContainer.Resolve<IUser>())
                 .ForMember(iUser => iUser.Id, mapper => mapper.MapFrom(src => src.Id));
+
+            //CreateMap<IFacility, Facility>()
+            //   .ForMember(facility => facility.FacilityId, mapper => mapper.MapFrom(src => src.Id));
+
+            //CreateMap<Facility, IFacility>()
+            //    .ConstructUsing(dbEntity => DependencyContainer.Resolve<IFacility>())
+            //    .ForMember(iFacility => iFacility.Id, mapper => mapper.MapFrom(src => src.FacilityId));
+
+            CreateMap<IApartment, Apartment>()
+               .ForMember(apartment => apartment.UserId, mapper => mapper.MapFrom(src => src.HouseHolder.Id))
+               .ForMember(apartment => apartment.Facilities, mapper => mapper.MapFrom(src => src.Facilities));
+
+               //.ForMember(
+               // apartment => apartment.ApartmentFacilities,
+               // mapper => mapper.MapFrom(src => src.Facilities.Select(ifuc => new ApartmentFacility
+               //     { ApartmentId = src.Id, FacilityId = ifuc.Id})));
+
+            CreateMap<Apartment, IApartment>()
+                .ConstructUsing(dbEntity => DependencyContainer.Resolve<IApartment>())
+                .ForMember(iApartment => iApartment.Id, mapper => mapper.MapFrom(src => src.Id))
+                .ForMember(iApartment => iApartment.Facilities, mapper => mapper.MapFrom(src => src.Facilities));
+
+
+            CreateMap<IPicture, Picture>();
+
+            CreateMap<Picture, IPicture>()
+                .ConstructUsing(dbEntity => DependencyContainer.Resolve<IPicture>());
+
+            CreateMap<IAvailableDate, AvailableDate>();
+
+            CreateMap<AvailableDate, IAvailableDate>()
+                .ConstructUsing(dbEntity => DependencyContainer.Resolve<IAvailableDate>());
 
             //TODO: add more mapping for other entities
         }
--- BookAndRent/BookAndRent/Mapping/ViewToModelMapper.cs
+++ BookAndRent/BookAndRent/Mapping/ViewToModelMapper.cs
@@ -9,7 +9,30 @@ namespace BookAndRent.Mapping
         public ViewToModelMapper()
         {
             CreateMap<Views.ViewModels.UserRegistration, IUser>()
-                .ConstructUsing(dbEntity => DependencyContainer.Resolve<IUser>());
+                .ConstructUsing(viewEntity => DependencyContainer.Resolve<IUser>());
+
+            CreateMap<IUser, Views.ViewModels.UserRegistration>();
+
+            CreateMap<Views.ViewModels.CreateApartment, IApartment>()
+                .ConstructUsing(viewEntity => DependencyContainer.Resolve<IApartment>());
+
+            CreateMap<IApartment, Views.ViewModels.CreateApartment>();
+
+
+            //CreateMap<IFacility, Views.ViewModels.Facility>();
+
+            //CreateMap<Views.ViewModels.Facility, IFacility>()
+            //    .ConstructUsing(viewEntity => DependencyContainer.Resolve<IFacility>());
+
+            CreateMap<IPicture, Views.ViewModels.ApartmentPicture>();
+
+            CreateMap<Views.ViewModels.ApartmentPicture, IPicture>()
+                .ConstructUsing(viewEntity => DependencyContainer.Resolve<IPicture>());
+
+            CreateMap<IAvailableDate, Views.ViewModels.AvailableDates>();
+
+            CreateMap<Views.ViewModels.AvailableDates, IAvailableDate>()
+                .ConstructUsing(viewEntity => DependencyContainer.Resolve<IAvailableDate>());
 
             //TODO: add more mapping for other entities
 
